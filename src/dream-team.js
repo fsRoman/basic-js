98cdb12c671898159e16d6d90a7232a42f6db5c0
fsRoman <51918514+fsRoman@users.noreply.github.com>
2021-02-27 19:22:35 +0300

    feat: work with dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,10 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+// module.exports = 
+function createDreamTeam(members) {
+  return members;
 };
+
+
+console.log('createDreamTeam(ADMM): ', createDreamTeam(['Matt', 'Ann', 'Dmitry', 'Max']));
+console.log('createDreamTeam(LOO): ', createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]));
