faebc5f4c96405954cd600ed48684864ebc53b53
fsRoman <51918514+fsRoman@users.noreply.github.com>
2021-02-28 20:14:09 +0300

    feat: implement tranform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,38 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if(!(arr instanceof Array)){throw 'Error'}
+  const res = [];
+  let [ignore, double] = [false, false];
+  arr.map((v, i) => {
+    if(ignore == true){
+      ignore = false
+      if(arr[i + 1] == '--discard-prev' || arr[i + 1] == '--double-prev'){
+        ignore = true
+      };
+      return;
+    };
+    if(double){
+      res.push(v, v);
+      double = false;
+      return;
+    };
+    switch(v){
+      case '--discard-next':
+        ignore = true;
+        break;
+      case '--discard-prev':
+        if(i > 0){res.pop()};
+        break;
+      case '--double-next':
+        double = true;
+        break;
+      case '--double-prev':
+        if(i > 0){res.push(arr[i - 1])};
+        break;
+      default:
+        res.push(v);
+    };
+  })
+  return res
 };
