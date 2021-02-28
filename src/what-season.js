c4d96f87740246d18d2a44b93891f0be14403254
fsRoman <51918514+fsRoman@users.noreply.github.com>
2021-02-28 20:39:03 +0300

    fix: success very tricky test

--- src/what-season.js
+++ src/what-season.js
@@ -4,6 +4,7 @@ module.exports = function getSeason(date) {
   if(!date){
     return 'Unable to determine the time of year!';
   }else if (Object.getPrototypeOf(date) === Date.prototype){
+    date.valueOf()
     const seasons = ['winter', 'spring', 'summer', 'autumn (fall)', 'winter'];
     return seasons[Math.floor((date.getMonth() + 1) / 3)];
   }else{
