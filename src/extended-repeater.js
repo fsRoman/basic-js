d4e8818985d75fa5add899423eb0d83fe13a27be
fsRoman <51918514+fsRoman@users.noreply.github.com>
2021-03-02 00:33:20 +0300

    feat: work with repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,23 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  console.log(str, options);
+  str = str.toString()
+  if(!options.separator){options.separator = '+'}
+  if(!options.additionSeparator){options.additionSeparator = '|'}
+  if(options.addition){
+    options.addition = options.addition.toString();
+    while(options.additionRepeatTimes > 0){
+      options.addition += `${options.additionSeparator}${options.addition}`
+      --options.additionRepeatTimes
+    }
+  }
+  str += `${options.addition ? options.addition : ''}`
+  while(options.repeatTimes > 0){
+    str += `${options.separator}${str}`
+    --options.repeatTimes
+  }
+  console.log(str, options);
+  return str
 };
   
--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -2,17 +2,9 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
   calculateDepth(arr) {
-    let depth = 0
-    function rec(arr){
-      if(arr instanceof Array){
-        depth++
-        arr.forEach(v => {
-          return depth + rec(v)
-        })
-      }
-    }
-    rec(arr)
-    console.log(depth, arr);
-    return depth
+    return Array.isArray(arr) ? 
+      1 + Math.max(...arr.map(this.calculateDepth)) :
+      0;
   }
+  
 };
